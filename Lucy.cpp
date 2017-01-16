a1d8291de29d4c84a0e6c4928b4c3f53f40c9f60
DerMilenius <mrottyseus@gmail.com>
2017-01-16 22:49:00 +0100

    Allgmeine Verbesserung
    Die Nutzung mit dem Atom-Editor wurde verbessert. Hello World wurde
    angepasst.

--- Lucy.cpp
+++ Lucy.cpp
@@ -1,7 +1,8 @@
 #include <iostream>                                     // Ein- und Ausgabebibliothek
 
-int main(){                                             // Hauptfunktion
+int main(){
+  int test = 0;                                     // Hauptfunktion
   std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe
-
+  std::cin >> test;
   return 0;                                           // Optionale Rückgabe an das Betriebssystem
 }
